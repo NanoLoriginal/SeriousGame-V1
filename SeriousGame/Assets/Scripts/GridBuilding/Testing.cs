54a6c19d31bbc6d26afa15cf0b7beb32cd1037a5
Nano <kyliann.faggian@gmail.com>
2022-12-06 17:39:26 +0100

    gridXZ effectuée

--- SeriousGame/Assets/Scripts/GridBuilding/Testing.cs
+++ SeriousGame/Assets/Scripts/GridBuilding/Testing.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    private Grid grid;
+    private GridXZ grid;
+
+    [SerializeField] LayerMask mouseLayer;
+    [SerializeField] private Transform mouseVisualTransform;
 
     private void Start()
     {
-        grid = new Grid(4, 2, 10f, new Vector3(0, 0));
+        grid = new GridXZ(4, 2, 10f, new Vector3(0, 0));
     }
 
     private void Update()
     {
+
+        Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseLayer))
+        {
+            mouseVisualTransform.position = raycastHit.point;
+        }
+
+
         if (Input.GetMouseButtonDown(0))
         {
-            grid.SetValue(gridUtils.GetMouseWorldPosition(), 56);
+            grid.SetValue(mouseVisualTransform.position, 56);
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log(grid.GetValue(gridUtils.GetMouseWorldPosition()));
+            Debug.Log(grid.GetValue(mouseVisualTransform.position));
         }
     }
 }
